45639f4785b7fa3b2189bd43a4b386be0546c0c5
saku0601 <saku0601@users.noreply.github.com>
2025-05-19 21:38:55 +0900

    fix: remove export from authOptions for Vercel

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -6,7 +6,7 @@ import NextAuth from 'next-auth/next';
 
 const prisma = new PrismaClient();
 
-export const authOptions: NextAuthOptions = {
+const authOptions: NextAuthOptions = {
   providers: [
     CredentialsProvider({
       name: 'Credentials',
@@ -77,6 +77,4 @@ export const authOptions: NextAuthOptions = {
   debug: true,
 };
 
-const handler = NextAuth(authOptions);
-
-export { handler as GET, handler as POST }; 
+export default NextAuth(authOptions); 
